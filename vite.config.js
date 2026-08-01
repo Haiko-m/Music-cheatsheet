3be9e45b1f404bf0e6d4dbc56e9a11003b455e9c
Haiko-m <haik.o@hotmail.com>
2026-08-01 22:51:09 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
-export default defineConfig({
-  // Relative URLs allow deployment under any GitHub repository name.
-  base: "./",
-  plugins: [react(), tailwindcss()],
-});
